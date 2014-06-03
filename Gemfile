64016cf2c256db37f88adb4d3cff1c998e510cdb
Eric Kelly <heroiceric@gmail.com>
2014-06-03 15:54:17 -0400

    Build the app

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'sinatra-flash'
+gem 'omniauth-github'
+
+group :development, :test do
+  gem 'dotenv'
+end
--- app.rb
+++ app.rb
@@ -1,10 +1,63 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/flash'
+require 'omniauth-github'
+
+require 'dotenv'
+Dotenv.load
+
+require 'pry'
+
+configure do
+  enable :sessions
+
+  use OmniAuth::Builder do
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  end
+
+  set :users, {}
+end
+
+helpers do
+  def current_user
+    uid = session['user_uid']
+    settings.users[uid]
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+end
+
+def authorize!
+  unless signed_in?
+    flash[:notice] = "You need to sign in first."
+    redirect '/'
+  end
+end
 
 get '/' do
   erb :index
 end
 
 get '/secret_page' do
+  authorize!
+
   erb :secret_page
+end
+
+get '/auth/github/callback' do
+  auth = env['omniauth.auth']
+  uid = auth['uid']
+  email = auth['info']['email']
+
+  settings.users[uid] = email
+  session["user_uid"] = uid
+
+  redirect '/'
+end
+
+get '/sign_out' do
+  session["user_uid"] = nil
+  redirect '/'
 end
